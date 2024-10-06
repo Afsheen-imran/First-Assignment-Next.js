609ec1056aa288c1fb9bc3e050495b20a990b440
Afsheen Imran <afsheen6441@gmail.com>
2024-10-07 02:45:24 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,52 +12,52 @@ const Home = () => {
       </p>
       <div className={styles.services}>
         <div className={styles.serviceCard}>
-        <Image
+        {/* <Image
               className="object-cover object-center rounded mx-auto w-[300px] h-[400px]"
               alt="Search Engine Optimization"
               src={require("../public/images/seo.jpeg")}
               width={300}
               height={500}
-          />
+          /> */}
           <h3 className={styles.serviceTitle}>SEO Optimization</h3>
           <p className={styles.serviceDescription}>
             Enhance your visibility on search engines and attract quality traffic to your website.
           </p>
         </div>
         <div className={styles.serviceCard}>
-        <Image
+        {/* {/* <Image
               className="object-cover object-center rounded mx-auto w-[300px] h-[400px]"
               alt="Graphic Designing"
               src={require("../public/images/graphic-design.jpeg")}
               width={300}
-              height={500}
-          />
+                height={500} */}
+          
           <h3 className={styles.serviceTitle}>Graphic Designing</h3>
           <p className={styles.serviceDescription}>
             Captivate your audience with stunning visuals that convey your brand message effectively.
           </p>
         </div>
         <div className={styles.serviceCard}>
-        <Image
+        {/* <Image
               className="object-cover object-center rounded mx-auto w-[300px] h-[400px]"
               alt="Social Media Marketing"
               src={require("../public/images/social-media.jpeg")}
               width={300}
               height={500}
-          />
+          /> */}
           <h3 className={styles.serviceTitle}>Social Media Marketing</h3>
           <p className={styles.serviceDescription}>
             Build a strong online presence and engage with your customers through strategic campaigns.
           </p>
         </div>
         <div className={styles.serviceCard}>
-        <Image
+        {/* <Image
               className="object-cover object-center rounded mx-auto w-[300px] h-[400px]"
               alt="Video Editing"
               src={require("../public/images/video-editing.jpeg")}
               width={300}
               height={500}
-          />
+          /> */}
           <h3 className={styles.serviceTitle}>Video Editing</h3>
           <p className={styles.serviceDescription}>
             Create impactful video content that tells your story and resonates with your audience.
